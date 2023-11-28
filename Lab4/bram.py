e89b7c559151b0be58e5c2151b1399afede92d3b
Hibiki33 <jiajun9264@gmail.com>
2023-11-29 00:23:53 +0800

    read and write bram on zynq

---
+++ Lab4/bram.py
@@ -0,0 +1,155 @@
+# -*-coding: utf-8-*-
+import numpy as np
+import mmap
+import os, sys
+
+class BramConfig(object):
+    '''BRAM信息配置'''
+
+    def _construct_block_info(address, size, **offset) -> dict:
+        '''构造block信息
+
+            Args:
+                name: 块名称
+                address: 块起始地址
+                size: 块大小
+                offset: 偏移量，字典
+
+            Return:
+                返回字典，包含address, size, offset字段。
+                其中offset是一个字典，表示各块内偏移的用途
+        '''
+        info = {
+            'address': address,
+            'size': size,
+            'offset': offset
+        }
+        return info
+
+
+    block_info = {}
+    # 构建新逻辑块参考以下写法
+    # 若块内偏移量无特殊含义，则约定key为default，值为0，可根据实际需求修改
+    block_info['input'] = _construct_block_info(
+        address=0x40000000, size=32*1024,
+        **{'default': 0x0}
+    )
+    block_info['weight'] = _construct_block_info(
+        address=0x40020000, size=128*1024,
+        **{'default': 0x0}
+    )
+    block_info['output'] = _construct_block_info(
+        address=0x40040000, size=32*1024,
+        **{'default': 0x0}
+    )
+    block_info['ir'] = _construct_block_info(
+        address=0x40060000, size=4*1024,
+        **{'flag': 0x0, 'instr': 0x10}
+    )
+
+
+class BRAM(object):
+    '''实现对Bram读写的类，需要先配置BramConfig类'''
+    def __init__(self):
+        self.block_info = BramConfig.block_info
+        self.block_map = self._mapping('/dev/mem')
+
+    def __del__(self):
+        os.close(self.file)
+        for block_name, block_map in self.block_map.items():
+            block_map.close()
+
+    def _mapping(self, path):
+        self.file = os.open(path, os.O_RDWR | os.O_SYNC)
+
+        block_map = {}
+        for name, info in self.block_info.items():
+            # 构建块内存映射
+            block_map[name] = mmap.mmap(
+                self.file, 
+                info['size'],
+                flags=mmap.MAP_SHARED,
+                prot=mmap.PROT_READ | mmap.PROT_WRITE,
+                offset=info['address']
+            )
+        return block_map
+    
+    def write(self, data, block_name: str, offset='default'):
+        '''写入数据
+            由于数据位宽32bit，因此最好以4的倍数Byte写入(还不知道以1Byte单位写进去会有什么效果)
+
+            Args：
+                data: 输入的数据
+                block_name: BramConfig中配置的block_info的key值
+                offset: BramConfig中配置的offset字典key值
+        '''
+        map_ = self.block_map[block_name]
+
+        # print("Data: \n%s" % data)
+        offset_ = self.block_info[block_name]['offset'][offset]
+        map_.seek(offset_)
+
+        if isinstance(data, np.ndarray):
+            data = data.reshape(-1)
+        map_.write(data)
+
+    def read(self, len, block_name, offset='default', dtype=np.uint8) -> np.ndarray:
+        '''按字节依次从低字节读取
+
+            Args：
+                len: 读取数据长度，单位字节
+                block_name: BramConfig中配置的block_info的key值
+                offset: BramConfig中配置的offset字典key值
+                dtype: 要求数据按相应的格式输出，
+                        np.int8, np.int16, np.int32, np.int64,
+                        np.uint8, np.uint16, np.uint32, np.uint64
+            
+            Return:
+                np.adarray
+        '''
+
+        # print("Read data from BRAM via AXI_bram_ctrl_1")
+        map_ = self.block_map[block_name]
+
+        offset_ = self.block_info[block_name]['offset'][offset]
+        map_.seek(offset_)
+
+        data = []
+        for i in range(len):
+            data.append(map_.read_byte())
+
+        data = np.array(data, dtype=np.uint8)
+        data.dtype=dtype   # 按dtype整理数据
+
+        return data
+
+
+if __name__ == '__main__':
+    bram = BRAM()       # 创建BRAM类实例
+
+    # 向input块中写入int8类型的ndarray
+    data_wirte = np.random.randint(-1, 2, (8,4), dtype=np.int8)
+    bram.write(data_wirte, 'input')
+    data_read = bram.read(data_wirte.size, 'input', dtype=np.int8).reshape(8,4)
+
+    print('write data')
+    print(data_wirte)
+    print()
+    print('read data')
+    print(data_read)
+    print()
+
+    # 向ir块中写入flag信息
+    flag_00 = b"\x00\x00\x00\x00"
+    flag_01 = b"\x01\x00\x00\x00"   # 最左侧字节存储在低地址处，往右依次存储在更高地址
+
+    bram.write(flag_00, 'ir', offset='flag')
+    flag_00_read = bram.read(1, 'ir', offset='flag', dtype=np.int8)
+    bram.write(flag_01, 'ir', offset='flag')
+    flag_01_read = bram.read(1, 'ir', offset='flag', dtype=np.int8)
+    
+    print('write flag_00:', flag_00)
+    print('read flag_00:', flag_00_read)
+    print()
+    print('write flag_01:', flag_01)
+    print('read flag_01:', flag_01_read)
---
+++ Lab4/linux_zynq_7020/linux_zynq_7020.ip_user_files/sim_scripts/design_1/modelsim/compile.do
@@ -0,0 +1,42 @@
+vlib modelsim_lib/work
+vlib modelsim_lib/msim
+
+vlib modelsim_lib/msim/xilinx_vip
+vlib modelsim_lib/msim/axi_infrastructure_v1_1_0
+vlib modelsim_lib/msim/axi_vip_v1_1_6
+vlib modelsim_lib/msim/processing_system7_vip_v1_0_8
+vlib modelsim_lib/msim/xil_defaultlib
+
+vmap xilinx_vip modelsim_lib/msim/xilinx_vip
+vmap axi_infrastructure_v1_1_0 modelsim_lib/msim/axi_infrastructure_v1_1_0
+vmap axi_vip_v1_1_6 modelsim_lib/msim/axi_vip_v1_1_6
+vmap processing_system7_vip_v1_0_8 modelsim_lib/msim/processing_system7_vip_v1_0_8
+vmap xil_defaultlib modelsim_lib/msim/xil_defaultlib
+
+vlog -work xilinx_vip -64 -incr -sv -L axi_vip_v1_1_6 -L processing_system7_vip_v1_0_8 -L xilinx_vip "+incdir+/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/include" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/axi_vip_axi4pc.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/xil_common_vip_pkg.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/axi_vip_pkg.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/axi4stream_vip_if.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/axi_vip_if.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/clk_vip_if.sv" \
+"/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/hdl/rst_vip_if.sv" \
+
+vlog -work axi_infrastructure_v1_1_0 -64 -incr "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/ec67/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/2d50/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ip/design_1_processing_system7_0_0" "+incdir+/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/include" \
+"../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+
+vlog -work axi_vip_v1_1_6 -64 -incr -sv -L axi_vip_v1_1_6 -L processing_system7_vip_v1_0_8 -L xilinx_vip "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/ec67/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/2d50/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ip/design_1_processing_system7_0_0" "+incdir+/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/include" \
+"../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/dc12/hdl/axi_vip_v1_1_vl_rfs.sv" \
+
+vlog -work processing_system7_vip_v1_0_8 -64 -incr -sv -L axi_vip_v1_1_6 -L processing_system7_vip_v1_0_8 -L xilinx_vip "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/ec67/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/2d50/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ip/design_1_processing_system7_0_0" "+incdir+/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/include" \
+"../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/2d50/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+
+vlog -work xil_defaultlib -64 -incr "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/ec67/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ipshared/2d50/hdl" "+incdir+../../../../linux_zynq_7020.srcs/sources_1/bd/design_1/ip/design_1_processing_system7_0_0" "+incdir+/tools/Xilinx/Vivado/2019.2/data/xilinx_vip/include" \
+"../../../bd/design_1/ip/design_1_processing_system7_0_0/sim/design_1_processing_system7_0_0.v" \
+"../../../bd/design_1/sim/design_1.v" \
+
+vlog -work xil_defaultlib \
+"glbl.v"
+
